7676393b161d6617122350917876e11d35f81c0a
Ashoke95 <malakarashoke95@gmail.com>
2022-02-01 14:36:15 +0530

    ok

---
+++ Assignment/Myname.java
@@ -0,0 +1,18 @@
+package Ashoke;
+
+ class name
+{
+	void display() {
+		System.out.println("My name is Ashoke Malakar");
+	}
+}
+
+ class Myname {
+
+	public static void main(String[] args) {
+		
+		name obj=new name();
+		obj.display();
+	}
+
+}
