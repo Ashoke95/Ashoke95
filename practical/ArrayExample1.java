b426b568a82aee8c634d75a5ab104ff4ec840ed4
Ashoke95 <malakarashoke95@gmail.com>
2022-02-02 16:25:24 +0530

    ok

---
+++ practical/ArrayExample1.java
@@ -0,0 +1,17 @@
+package Ashoke;
+
+public class ArrayExample1 {
+
+	public static void main(String[] args) {
+		int a[]= {4,6,8,9,6,4};
+		
+		System.out.println("Array is ");
+		for(int i: a)//copy value of a into i ,this loop is called foreach loop
+		{
+	
+		System.out.print(i+" ");
+		
+		}			
+
+}
+}
