4e2f43cf8cdc0d3505be0bc73423edfa49ee4aa0
Ashoke95 <malakarashoke95@gmail.com>
2022-02-02 15:52:04 +0530

    ok

---
+++ practical/ArrayExample.java
@@ -0,0 +1,21 @@
+package Ashoke;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]=new int[5];// declearing array
+		a[0]=10;
+		a[1]=12;
+		a[2]=5;
+		a[3]=34;
+		a[4]=40;
+		System.out.println("Array is ");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+			
+	}
+
+}
