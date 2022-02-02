2e73ffb45cd8806be59ca197c5557434a19070ce
Ashoke95 <malakarashoke95@gmail.com>
2022-02-02 16:07:28 +0530

    ok

---
+++ practical/ArrayExample2.java
@@ -0,0 +1,19 @@
+package Ashoke;
+
+public class ArrayExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {1,3,5,7,8};//directly initilyzing 
+
+
+System.out.println("Array is ");
+for(int i=0;i<a.length;i++)
+{
+	System.out.print(a[i]+" ");
+}
+
+
+	}
+
+}
