993f24de21d55172e1644518a32402313cc5e9ce
Ashoke95 <malakarashoke95@gmail.com>
2022-02-03 19:29:13 +0530

    Ok

---
+++ Assignment/Array/Array4.java
@@ -0,0 +1,15 @@
+package Ashoke;
+
+public class Array4 {
+
+	public static void main(String[] args) {
+		
+		int [] intArr = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
+		 for (int i = 0; i < intArr . length ; i += 3) 
+		{ 
+		System . out . println (" Value : " + intArr [ i ]) ; 
+		} 
+
+	}
+
+}
