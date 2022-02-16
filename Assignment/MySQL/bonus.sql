e9624e0718d28e61d7919cb92df1e6ff998dc45a
Ashoke95 <malakarashoke95@gmail.com>
2022-02-16 18:00:03 +0530

    16-02-22

---
+++ Assignment/MySQL/bonus.sql
@@ -0,0 +1,17 @@
+use regis;
+desc bonus;
+SELECT * FROM regis.bonus;
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(1,'14-02-20 09.00.00', 4000);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(2,'16-02-22 05.06.10', 3400);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(3,'2022-02-16 05:09:22', 2300);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(4,'16-02-22 05.16.10', 3400);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(5,'16-02-22 05.18.10', 3400);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(6,'16-02-22 05.16.10', 1400);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(7,'16-02-22 05.26.10', 3300);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(8,'16-02-22 05.28.10', 3470);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(9,'16-02-22 05.30.10', 400);
+insert into bonus(Worker_ref_id,Bonus_date,Bonus_Amount)values(10,'16-02-22 05.45.10', 4000);
+
+
+
+select* from bonus;
