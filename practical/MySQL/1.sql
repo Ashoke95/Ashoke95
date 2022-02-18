31085cc1d4c4565d90b4222ff16e771e5527283e
Ashoke95 <malakarashoke95@gmail.com>
2022-02-18 16:44:34 +0530

    OK

--- practical/MySQL/1.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM regis.student;
-desc student;
-insert into student(id,name,roll,address) values(1,'ram',5,'haur');
-insert into student(id,name,roll,address) values(2,'shyam',4,'fgfdhgk');
-insert into student(id,name,roll,address) values(3,'arun',6,'fddadd');
-insert into student(id,name,roll,address) values(4,'rahul',9,'sff');
-insert into student(id,name,roll,address) values(5,'kamal',1,'yhf');
-select* from student;
