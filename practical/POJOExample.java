24559586328e0f437c9df6d2d836beba7b13eec2
Ashoke95 <malakarashoke95@gmail.com>
2022-03-15 09:09:42 +0530

    14-03-22

---
+++ practical/POJOExample.java
@@ -0,0 +1,27 @@
+package com;
+
+public class POJOExample {
+	private  int id;
+	private String name;
+	private String email;
+	int getId() {
+		return id;
+	}
+	void setId(int id) {
+		this.id = id;
+	}
+	String getName() {
+		return name;
+	}
+	void setName(String name) {
+		this.name = name;
+	}
+	String getEmail() {
+		return email;
+	}
+	void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
+
---
+++ practical/POJOMiainCalss.java
@@ -0,0 +1,15 @@
+package com;
+
+public class POJOMiainCalss {
+
+	public static void main(String[] args) {
+
+		POJOExample s=new POJOExample();
+s.setId(100);
+s.setName("Ashoke");
+s.setEmail("ashoke@gmail.com");
+
+System.out.println("name is  "+s.getName()+" \n id is  :"+s.getId()+"   \nemail is:  "+s.getEmail());
+	}
+
+}
