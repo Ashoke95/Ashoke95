38b1c21940c0df6502200668766fa9b7ade3fdb3
Ashoke95 <98313655+Ashoke95@users.noreply.github.com>
2022-03-30 02:16:59 -0700

    30-03-22

---
+++ Labactivity/InheritenceAssignment/Trainee.java
@@ -0,0 +1,20 @@
+
+public class Trainee extends Employee{
+     
+	public Trainee(long id, String Name, String address, long phone,double salary) {
+		super(id, Name, address, phone);
+
+		this.employeeId=id;
+		this.employeeName=Name;
+		this.employeeaddress=address;
+		this.employeephone=phone;
+		this.basicsalary=salary;
+		
+		
+		
+		
+	}
+	
+	
+
+}
